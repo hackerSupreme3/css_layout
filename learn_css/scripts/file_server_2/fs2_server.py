d1facc61f2af032363b66bf343a738cf5e216e95
hackerSupreme3 <nottherealgary5@gmail.com>
2021-08-05 22:36:42 +0200

    Established basic GET request on server. Keyboard interrupt works - but it runs very slow

--- learn_css/scripts/file_server_2/fs2_server.py
+++ learn_css/scripts/file_server_2/fs2_server.py
@@ -1,18 +1,38 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
+server_address = ('', 8000)
+
+class NewServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>This Works</title></head><body><p>This works</p></body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    myServer = HTTPServer(server_address, NewServer)
+    print("Serving started http://%s:%s", server_address[0], server_address[1])
 
     try:
-        httpd.serve_forever()
-        print('Serving at ', server_address)
+        myServer.serve_forever()
     except KeyboardInterrupt:
         pass
+    
+    print('Server jizzed out')
 
-    httpd.server_close()
-    print('Server stopped')
+#def run(server_class=HTTPServer, handler_class=BaseHTTPRequeerve_forever)
+    #server_address = ('', 8000)
+    #httpd = server_class(server_address, handler_class)
+
+    #try:
+    #    httpd.serve_forever()
+    #    print('Serving at ', server_address)
+    #except KeyboardInterrupt:
+    #    pass
+
+    #httpd.server_close()
+    #print('Server stopped')
 
 
-run()
+
 
